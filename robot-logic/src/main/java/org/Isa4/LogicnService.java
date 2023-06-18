cf1f5a0d66017616f87ab1d38028539279402d19
AleksandrIsa4 <isachenkov.am@yandex.ru>
2023-06-18 19:07:48 +0300

    feat: Каркас программы 0.011

---
+++ robot-logic/src/main/java/org/Isa4/LogicnService.java
@@ -0,0 +1,10 @@
+package org.Isa4;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicnService {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
