7038771bc4ca0a6937050ae8a1c54be19e146f5e
AleksandrIsa4 <isachenkov.am@yandex.ru>
2023-07-10 10:34:19 +0300

    feat: Добавлена получение информации о доступных активах в портфеле (через @Async) и запись инструмента для торговли по эндпоинтам. Версия 2

--- robot-logic/src/main/java/org/Isa4/service/InstrumentService.java
+++ robot-logic/src/main/java/org/Isa4/service/InstrumentService.java
@@ -8,6 +8,7 @@ import org.Isa4.exceptions.BadRequestException;
 import org.Isa4.mapper.InformationAccountMapper;
 import org.Isa4.model.InformationAccount;
 import org.Isa4.model.PositionInstrument;
+import org.Isa4.producer.ProducerLogic;
 import org.Isa4.repository.InformationAccountRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -34,17 +35,13 @@ public class InstrumentService {
 
     private final PositionInstrumentService positionInstrumentService;
 
-    private final ObjectMapper objectMapper;
-
-    private final KafkaTemplate<Long, String> kafkaTemplate;
-
-    private static final String TOPIC_GETITEM = "getItem";
+    private final ProducerLogic producerLogic;
 
     public List<PositionInstrument> info(InformationAccount dto) {
         informationAccountRepository.deleteAll();
         InformationAccount informationAccount = informationAccountRepository.save(dto);
         InformationAccountResponse informationAccountResponse = InformationAccountMapper.toDto(informationAccount);
-        LocalDateTime ldt = sendInformationTool(informationAccountResponse);
+        LocalDateTime ldt = producerLogic.sendInformationTool(informationAccountResponse);
         try {
             while (LocalDateTime.now().isBefore(ldt.plusSeconds(KAFKA_DELAY_SECOND))) {
                 CompletableFuture<List<PositionInstrument>> listCompletableFuture = positionInstrumentService.getAllInstrumets();
@@ -57,17 +54,5 @@ public class InstrumentService {
             throw new BadRequestException("Исключение в методе info в классе InstrumentService из-за задержки получения сообщения");
         }
         throw new BadRequestException("Исключение в методе info в классе InstrumentService из-за задержки получения сообщения");
-    }
-
-    private LocalDateTime sendInformationTool(InformationAccountResponse dto) {
-        try {
-            String json = objectMapper.writeValueAsString(dto);
-            SendResult<Long, String> sendResult = kafkaTemplate.send(TOPIC_GETITEM, json).get();
-            LocalDateTime ldt = Instant.ofEpochMilli(sendResult.getRecordMetadata().timestamp())
-                    .atZone(ZoneId.systemDefault()).toLocalDateTime();
-            return ldt;
-        } catch (JsonProcessingException | ExecutionException | InterruptedException e) {
-            throw new BadRequestException("Исключение в методе sendInformationToo в классе ProducerLogicl");
-        }
     }
 }
